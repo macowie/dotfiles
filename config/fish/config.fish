c564ce41ed7015aae4da4258ffcc72be6aaa9be7
Matthew Cowie <matthew@cowie.me>
2016-12-16 11:38:07 -0600

    Add fish's chips

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,3 +3,6 @@ set -g theme_nerd_fonts yes
 
 # rbenv
 status --is-interactive; and source (rbenv init -|psub)
+
+# chips
+if [ -e ~/.config/chips/build.fish ] ; source ~/.config/chips/build.fish ; end
