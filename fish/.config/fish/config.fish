0eb712619c9ec9feaf276361d669420fe53d4e9c
Matthew Cowie <matthew@cowie.me>
2018-06-22 14:56:10 -0500

    Add mysql@5.7 to path because that's the version needed for the (thankfully) one mysql project we have

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,9 @@
 source ~/.profile
 set -g theme_nerd_fonts yes
-fish_vi_key_bindings
 
 # rbenv
 status --is-interactive; and source (rbenv init -|psub)
 
 # chips
 if [ -e ~/.config/chips/build.fish ] ; source ~/.config/chips/build.fish ; end
+set -g fish_user_paths "/usr/local/opt/mysql@5.7/bin" $fish_user_paths
