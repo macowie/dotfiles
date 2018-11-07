d70c989a6d03aa857635f0d1650d7d98c98a0e26
Matthew Cowie <matthew@cowie.me>
2018-11-06 18:54:09 -0600

    Migrate from chips (still works but maybe abandoned and hard to install) to fundle

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,19 @@
 source ~/.profile
 set -g theme_nerd_fonts yes
 
+switch (uname)
+case Linux
+  alias flushdns="sudo systemd-resolve --flush-caches"
+case Darwin
+  alias flushdns="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
+end
+
+alias gst="git status" 
+
 # rbenv
 status --is-interactive; and source (rbenv init -|psub)
 
-# chips
-if [ -e ~/.config/chips/build.fish ] ; source ~/.config/chips/build.fish ; end
-set -g fish_user_paths "/usr/local/opt/mysql@5.7/bin" $fish_user_paths
+if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+fundle plugin "simnalamburt/shellder"
+fundle plugin "simnalamburt/fish-sensible"
+fundle init
