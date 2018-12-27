af156229b044eac33944888cb0283b1509f8f088
Matthew Cowie <matthew@cowie.me>
2018-12-26 20:16:31 -0600

    Make fish config standalone.
    Fish on linux wasn't liking PATH stuff in generic profile, not worth the trouble anyway

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,18 +1,22 @@
-source ~/.profile
+source ~/.credentials
 set -g theme_nerd_fonts yes
+set -g nvim $EDITOR
+
+set -U fish_user_paths ~/.rbenv/bin ~/.cargo/bin $fish_user_paths
 
 switch (uname)
-case Linux
-  alias flushdns="sudo systemd-resolve --flush-caches"
-case Darwin
-  alias flushdns="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
+  case Linux
+    alias flushdns="sudo systemd-resolve --flush-caches"
+  case Darwin
+    alias flushdns="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
 end
 
-alias gst="git status" 
+alias gst="git status"
 
 # rbenv
 status --is-interactive; and source (rbenv init -|psub)
 
+# Fundle
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 fundle plugin "simnalamburt/shellder"
 fundle plugin "simnalamburt/fish-sensible"
