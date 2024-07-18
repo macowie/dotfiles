99d0a1a94abe3fb9cc7b6f3da8f67c2f5297a433
Madeline Cowie <madeline@cowie.me>
2024-07-18 09:05:07 -0500

    Easier hardtime

--- nvim/.config/nvim/lua/plugins/hardtime.lua
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -1,5 +1,9 @@
 return {
   "m4xshen/hardtime.nvim",
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  opts = {},
+  opts = {
+    disable_mouse = false,
+    restriction_mode = "hint",
+    max_count = 4,
+  },
 }
